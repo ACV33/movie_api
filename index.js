afe0746151d8394142b7fb8fee02c8c3d1b64615
ACV33 <101817262+ACV33@users.noreply.github.com>
2022-08-11 18:12:27 -0400

    updated changes to index.js

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-const http = require('http');
+const express = require('express'),
+    app = express();
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
+    morgan = require('morgan');
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Welcome to my book club!\n');
-}).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');
+app.use(bodyParser.json());
+
+let users = [
+
+]
+
+let movies = [
+    {
+        title: 'Stand By Me',
+        description: 'After the death of one of his friends, a writer recounts a childhood journey with his friends to find the body of a missing boy.'
+    },
+    {
+        title: 'Goodfellas',
+        description: 'The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.'
+    },
+    {   
+        title: 'Girls Just Want to Have Fun',
+        description: 'Janey is new in town and soon meets Lynne, who shares her passion for dancing in general and "Dance TV" in particular.'
+    },
+     {
+        title: 'Sex and the City',
+        description: 'A New York City writer on sex and love is finally getting married to her Mr. Big. But her three best girlfriends must console her after one of them inadvertently leads Mr. Big to jilt her.'  
+     },
+      {
+        title: 'Edward Scissorhands',
+        description: 'An artificial man, who was incompletely constructed and has scissors for hands, leads a solitary life. Then one day, a suburban lady meets him and introduces him to her world.'
+      }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Here is a few of my top favorite movies');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(404).send('Something went wrong!');
+  });
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
